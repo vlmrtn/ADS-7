745d0f77a0c4524bdbc82b8f69b63d33feb6dc9c
vlmrtn <nmartin_02@mail.ru>
2025-05-19 23:53:32 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -13,8 +13,9 @@ class Train {
   Cage *first; // точка входа в поезд (первый вагон)
  public:
   Train();
+ ~Train();
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+  int getOpCount() const;         // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
